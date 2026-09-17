bade93b3119d063764c2ede22e25338267fa57d5
GIG <liuhong.bing@qq.com>
2026-09-17 12:10:27 +0800

    重构

--- src/main/java/first/lyra/api/LyraAPI.java
+++ src/main/java/first/lyra/api/LyraAPI.java
@@ -2,19 +2,26 @@ package first.lyra.api;
 
 import first.lyra.client.dynamicLight.DynamicLightDispatcher;
 import first.lyra.common.attachment.DamageInfoData;
+import first.lyra.utils.ParticleHelper;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
-public final class LyraAPI {
+public interface LyraAPI {
 
-    private LyraAPI() {
+    static LyraHelper entity(Player player) {
+        return LyraHelper.get(player);
     }
 
-    public static DamageInfoData.DamageInfoBuilder damageInfo(Level level) {
+    static ParticleHelper particle(Level level) {
+        return ParticleHelper.create(level);
+    }
+
+    static DamageInfoData.DamageInfoBuilder damageInfo(Level level) {
         return DamageInfoData.build(level);
     }
 
-    public static void dynamicLight(Vec3 pos, int light) {
+    static void light(Vec3 pos, int light) {
         DynamicLightDispatcher.addLightSources(pos, light);
     }
 }
